b9d2ae4f44b18d04659b169a6978486ec9020069
MatiaLandry <matialandry367@gmail.com>
2025-03-07 10:32:44 -0800

    WIP:dashboard

--- App/app/src/main/java/com/example/recommender/MainScreen.java
+++ App/app/src/main/java/com/example/recommender/MainScreen.java
@@ -1,29 +1,50 @@
 package com.example.recommender;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 import androidx.fragment.app.Fragment;
 
-public class MainScreen extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+public class MainScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Fragment homefrag = new HomeFragment();
+        Fragment groupfrag = new GroupsFragment();
+        Fragment addfrag = new AddFragment();
+        Fragment ratefrag = new RateFragment();
+        Fragment savedfrag = new SavedFragment();
+        setCurrentFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                setCurrentFragment(homefrag);
+            } else if (id == R.id.groups) {
+                setCurrentFragment(groupfrag);
+            } else if (id == R.id.add) {
+                setCurrentFragment(addfrag);
+            }
+            return true;
         });
+
     }
+
+    // Helper method to switch fragments in the container
     private void setCurrentFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, fragment).commit();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
     }
 }
