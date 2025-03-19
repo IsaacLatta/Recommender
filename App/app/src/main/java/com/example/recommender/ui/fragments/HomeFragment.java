a4b315163e5414512c4913dc49222786bdce6f9a
MatiaLandry <matialandry367@gmail.com>
2025-03-18 21:25:18 -0700

    WIP: home fragment. Integrating changes from the groups tab

--- App/app/src/main/java/com/example/recommender/ui/fragments/HomeFragment.java
+++ App/app/src/main/java/com/example/recommender/ui/fragments/HomeFragment.java
@@ -6,19 +6,38 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.recommender.Controller;
 import com.example.recommender.R;
 
 
 public class HomeFragment extends Fragment {
 
+
+    private Button searchButton;
+    private EditText searchBarText;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        searchButton = view.findViewById(R.id.search_button);
+        searchBarText = view.findViewById(R.id.search_bar);
+
+        searchButton.setOnClickListener(v ->{
+            String search_input = searchBarText.getText().toString().trim();
+            Log.d("HOME_SEARCH_", search_input);
+           // Controller.getInstance().searchBook(search_input);
+        });
+        return view;
+
     }
 
 }
